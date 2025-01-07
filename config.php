d4c6f83bd93dd8a1a3b549abe36669d5696d8fac
Richard <rvdveldt@gmail.com>
2025-01-07 12:06:14 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'localhost');               // Of IP-adres zoals '127.0.0.1'
+define('DB_USERNAME', 'database-username');     // Databasegebruiker
+define('DB_PASSWORD', 'database-password');     // Wachtwoord
+define('DB_NAME', 'database-name');             // Database naam
+define('DB_PORT', 3307);                        // Database Port (If Needed?)
+?>
