043f938d55cfd34dc96a1ab2961cdde3278447e1
Richard <rvdveldt@gmail.com>
2025-01-18 08:13:34 +0000

    Comment explanation in config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // PHP Script with Database Credentials to edit the Messages for the Arduino UNO R4 Wifi
 // (c)2025 Richard, webwings.nl
 
-define('DB_SERVER', 'localhost');                 // Or an IP-adress like '127.0.0.1'
+define('DB_SERVER', 'localhost');                 // Or a remote URL, or an IP-adress.
 define('DB_USERNAME', 'your-database-username');  // Database User-account
 define('DB_PASSWORD', 'your-database-password');  // Database User Password
 define('DB_NAME', 'your-database-name');          // Database Name
